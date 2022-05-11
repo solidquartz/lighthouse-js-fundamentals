73b96ce03a816aa6d8a0356e04ced95cbd5a0c73
Aky Mac Donald <kotoko@Kotoko.local>
2022-05-11 17:57:04 -0400

    Add area project

---
+++ area.js
@@ -0,0 +1,39 @@
+//calculates area of rectangle
+function calculateRectangleArea(length, width) {
+  if (length <= 0 || width <= 0) {
+    return undefined;
+  } else {
+    return length * width;
+  }
+}
+
+//calculates area of triangle
+function calculateTriangleArea(base, height) {
+  if (base <= 0 || height <= 0) {
+    return undefined;
+  } else {
+    return (base * height) / 2;
+  }
+}
+
+//calculates area of circle
+function calculateCircleArea(radius) {
+  if (radius <= 0) {
+    return undefined;
+  } else {
+    return Math.PI * Math.pow(radius, 2);
+  }
+}
+
+//checks function
+console.log(calculateRectangleArea(10, 5)); // should print 50
+console.log(calculateRectangleArea(1.5, 2.5)); // should print 3.75
+console.log(calculateRectangleArea(10, -5)); // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
