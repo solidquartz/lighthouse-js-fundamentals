87a99830f4ff4a7831d8c8abeb913ce5a15ce6dc
Aky Mac Donald <kotoko@Kotoko.local>
2022-05-12 16:24:08 -0400

    Add ice cream array examples

---
+++ ice-cream.js
@@ -0,0 +1,23 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+//adds a new flavour
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+
+//logs the first flavour
+console.log(iceCreamFlavours[0]);
+
+//logs the last flavour
+console.log(iceCreamFlavours[5]);
+
+//logs number of flavours
+console.log(iceCreamFlavours.length);
