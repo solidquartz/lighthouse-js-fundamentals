30def46136b0a8b2e42ec1bb130669855cb69caf
Aky Mac Donald <kotoko@Kotoko.local>
2022-05-12 16:23:43 -0400

    Add while loop

--- packing-list.js
+++ packing-list.js
@@ -9,7 +9,16 @@ const packingList = [
   "cooking utensils",
 ];
 
+//used for loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//uses while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
